16e4aeb7f612391a99c3e86fd7794b64cdef3860
mahealomwebdev1 <61292677+mahealomwebdev1@users.noreply.github.com>
2020-03-15 14:03:57 +0600

    first commit

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import users from '../userdata/UsersList'
 import { faPeopleCarry } from '@fortawesome/free-solid-svg-icons';
+import './Friend.css'
 
 const Friend = (props) => {
     const friend= props.friend;
@@ -10,9 +11,9 @@ const Friend = (props) => {
         <div>
             <h4>Total User:{users.length}</h4>
             <h5> Friends: {friend.length}</h5>
-            <h5>Added Friends:
+            <p className="adding-friend" >Added Friends:
                 <br/>
-                {props.friend} </h5>
+                {props.friend} </p>
                
             
            
